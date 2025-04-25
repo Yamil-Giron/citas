56126d7f4e3c6f23689bd9364f2d8c97d51e130a
Yamil <yamilgiron98@gmail.com>
2025-04-24 21:20:35 -0400

    Inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,16 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'inicio',
+    loadComponent: () => import('./inicio/inicio.page').then( m => m.InicioPage)
+  },
+  {
+    path: 'gestion',
+    loadComponent: () => import('./gestion/gestion.page').then( m => m.GestionPage)
+  },
+  {
+    path: 'configuracion',
+    loadComponent: () => import('./configuracion/configuracion.page').then( m => m.ConfiguracionPage)
+  },
 ];
