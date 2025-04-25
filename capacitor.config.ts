b0b46a590607ef6a54f302d2bc451bb0faa4970b
Yamil <yamilgiron98@gmail.com>
2025-04-24 21:18:11 -0400

    inicial

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.citas',
+  appName: 'citas',
+  webDir: 'www'
+};
+
+export default config;
